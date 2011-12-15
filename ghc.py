3d14cafaf2040943e16cdd69de66c2e05f2fe380
Nathan Howell <nhowell@alphaheavy.com>
2011-12-15 00:46:00 -0800

    Reorganizing

--- ghc.py
+++ ghc.py
@@ -1,178 +1,251 @@
 import lldb
+import ghc_map
 
 # (lldb) command script import '/Source/ghc-lldb/ghc.py'
 
-closure_name_map = {0:  'INVALID_OBJECT'
-                   ,1:  'CONSTR'
-                   ,2:  'CONSTR_1_0'
-                   ,3:  'CONSTR_0_1'
-                   ,4:  'CONSTR_2_0'
-                   ,5:  'CONSTR_1_1'
-                   ,6:  'CONSTR_0_2'
-                   ,7:  'CONSTR_STATIC'
-                   ,8:  'CONSTR_NOCAF_STATIC'
-                   ,9:  'FUN'
-                   ,10: 'FUN_1_0'
-                   ,11: 'FUN_0_1'
-                   ,12: 'FUN_2_0'
-                   ,13: 'FUN_1_1'
-                   ,14: 'FUN_0_2'
-                   ,15: 'FUN_STATIC'
-                   ,16: 'THUNK'
-                   ,17: 'THUNK_1_0'
-                   ,18: 'THUNK_0_1'
-                   ,19: 'THUNK_2_0'
-                   ,20: 'THUNK_1_1'
-                   ,21: 'THUNK_0_2'
-                   ,22: 'THUNK_STATIC'
-                   ,23: 'THUNK_SELECTOR'
-                   ,24: 'BCO'
-                   ,25: 'AP'
-                   ,26: 'PAP'
-                   ,27: 'AP_STACK'
-                   ,28: 'IND'
-                   ,29: 'IND_PERM'
-                   ,30: 'IND_STATIC'
-                   ,31: 'RET_BCO '
-                   ,32: 'RET_SMALL'
-                   ,33: 'RET_BIG'
-                   ,34: 'RET_DYN'
-                   ,35: 'RET_FUN '
-                   ,36: 'UPDATE_FRAME'
-                   ,37: 'CATCH_FRAME'
-                   ,38: 'UNDERFLOW_FRAME'
-                   ,39: 'STOP_FRAME'
-                   ,40: 'BLOCKING_QUEUE'
-                   ,41: 'BLACKHOLE'
-                   ,42: 'MVAR_CLEAN'
-                   ,43: 'MVAR_DIRTY'
-                   ,44: 'ARR_WORDS'
-                   ,45: 'MUT_ARR_PTRS_CLEAN'
-                   ,46: 'MUT_ARR_PTRS_DIRTY'
-                   ,47: 'MUT_ARR_PTRS_FROZEN0'
-                   ,48: 'MUT_ARR_PTRS_FROZEN'
-                   ,49: 'MUT_VAR_CLEAN'
-                   ,50: 'MUT_VAR_DIRTY'
-                   ,51: 'WEAK'
-                   ,52: 'PRIM'
-                   ,53: 'MUT_PRIM'
-                   ,54: 'TSO'
-                   ,55: 'STACK '
-                   ,56: 'TREC_CHUNK'
-                   ,57: 'ATOMICALLY_FRAME'
-                   ,58: 'CATCH_RETRY_FRAME'
-                   ,59: 'CATCH_STM_FRAME'
-                   ,60: 'WHITEHOLE'}
+class Closure(object):
+    def __init__(self, debugger, obj):
+        self.debugger = debugger
+        self.obj = obj
+        self.payload = {}
 
-closure_type_map = {'CONSTR':               'StgClosure_'
-                   ,'CONSTR_1_0':           'StgClosure_'
-                   ,'CONSTR_0_1':           'StgClosure_'
-                   ,'CONSTR_2_0':           'StgClosure_'
-                   ,'CONSTR_1_1':           'StgClosure_'
-                   ,'CONSTR_0_2':           'StgClosure_'
-                   ,'CONSTR_STATIC':        'StgClosure_'
-                   ,'CONSTR_NOCAF_STATIC':  'StgClosure_'
-                   ,'FUN':                  'StgClosure_'
-                   ,'FUN_1_0':              'StgClosure_'
-                   ,'FUN_0_1':              'StgClosure_'
-                   ,'FUN_2_0':              'StgClosure_'
-                   ,'FUN_1_1':              'StgClosure_'
-                   ,'FUN_0_2':              'StgClosure_'
-                   ,'FUN_STATIC':           'StgClosure_'
-                   ,'THUNK':                'StgThunk'
-                   ,'THUNK_1_0':            'StgThunk'
-                   ,'THUNK_0_1':            'StgThunk'
-                   ,'THUNK_2_0':            'StgThunk'
-                   ,'THUNK_1_1':            'StgThunk'
-                   ,'THUNK_0_2':            'StgThunk'
-                   ,'THUNK_STATIC':         'StgThunk'
-                   ,'THUNK_SELECTOR':       'StgSelector'
-                   ,'BCO':                  'StgBCO'
-                   ,'AP':                   'StgAP'
-                   ,'PAP':                  'StgPAP'
-                   ,'AP_STACK':             'StgAP_STACK'
-                   ,'IND':                  'StgInd'
-                   ,'IND_PERM':             'StgInd'
-                   ,'IND_STATIC':           'StgIndStatic'
-                   # ,'RET_BCO ':
-                   # ,'RET_SMALL':
-                   # ,'RET_BIG':
-                   # ,'RET_DYN':
-                   # ,'RET_FUN ':
-                   ,'UPDATE_FRAME':         'StgUpdateFrame'
-                   ,'CATCH_FRAME':          'StgCatchFrame'
-                   ,'UNDERFLOW_FRAME':      'StgUnderflowFrame'
-                   ,'STOP_FRAME':           'StgStopFrame'
-                   ,'BLOCKING_QUEUE':       'StgBlockingQueue'
-                   ,'BLACKHOLE':            'StgInd'
-                   ,'MVAR_CLEAN':           'StgMVar'
-                   ,'MVAR_DIRTY':           'StgMVar'
-                   ,'ARR_WORDS':            'StgArrWords'
-                   ,'MUT_ARR_PTRS_CLEAN':   'StgMutArrPtrs'
-                   ,'MUT_ARR_PTRS_DIRTY':   'StgMutArrPtrs'
-                   ,'MUT_ARR_PTRS_FROZEN0': 'StgMutArrPtrs'
-                   ,'MUT_ARR_PTRS_FROZEN':  'StgMutArrPtrs'
-                   ,'MUT_VAR_CLEAN':        'StgMutVar'
-                   ,'MUT_VAR_DIRTY':        'StgMutVar'
-                   ,'WEAK':                 'StgWeak'
-                   # ,'PRIM':
-                   # ,'MUT_PRIM':
-                   ,'TSO':                  'StgTSO'
-                   # ,'STACK ':
-                   ,'TREC_CHUNK':           'StgTRecChunk'
-                   ,'ATOMICALLY_FRAME':     'StgAtomicallyFrame'
-                   ,'CATCH_RETRY_FRAME':    'StgCatchRetryFrame'
-                   ,'CATCH_STM_FRAME':      'StgCatchSTMFrame'
-                   ,'WHITEHOLE':            'StgInd'}
+    def __str__(self):
+        return str(self.info_table())
 
-def closure_type_name(tag):
-    name = closure_name_map[tag]
-    return closure_type_map[name]
+    def __repr__(self):
+        return str(self.info_table())
 
-def untag_closure(obj):
-    # TODO: untag for integers and pointers
-    closure_type = lldb.debugger.GetSelectedTarget().FindFirstType('StgClosure_')
-    return obj.CreateValueFromAddress(obj.GetName() + '_closure', obj.GetValueAsUnsigned() & ~7, closure_type)
+    def reify(self):
+        info = self.info_table().info_table
+        ptrs = info.GetValueForExpressionPath('.layout.payload.ptrs').GetValueAsUnsigned()
+        nptrs = info.GetValueForExpressionPath('.layout.payload.nptrs').GetValueAsUnsigned()
 
-def get_info_table_from_closure(obj):
-    target = lldb.debugger.GetSelectedTarget()
-    # lldb doesn't support negative subscripts, this would be cleaner as '.header.info[-1]'
-    info_table = obj.GetValueForExpressionPath('.header.info')
-    info_table_sym = target.ResolveSymbolContextForAddress(info_table.Dereference().GetAddress(), lldb.eSymbolContextSymbol).GetSymbol()
-    # sym.GetStartAddress().GetLoadAddress(target)
-    # cast it back to the info table
-    stg_info_table_type = target.FindFirstType('StgInfoTable_')
-    return info_table.CreateValueFromAddress(info_table_sym.GetName(), info_table.GetValueAsUnsigned() - 16, stg_info_table_type)
+        # lldb does bounds checks on array subscripts, which doesn't work with GHC's closure types
+        # so strip the StgClosure[1] type of the array size, leaving StgClosure* (which is not bounds checked)
+        payload = self.obj.GetValueForExpressionPath('.payload[0]').AddressOf()
 
-def get_con_desc(info):
-    target = lldb.debugger.GetSelectedTarget()
-    stg_con_info_table_type = target.FindTypes('StgConInfoTable_').GetTypeAtIndex(0)
-    # stg_con_info_table_type = target.FindFirstType('StgConInfoTable_') # waiting on bug 11574
-    con_info = info.Cast(stg_con_info_table_type)
-    con_info_ptr = con_info.AddressOf()
-    char_type = info.GetType().GetBasicType(lldb.eBasicTypeChar)
-    char_ptr_type = char_type.GetPointerType()
-    base = con_info_ptr.GetValueForExpressionPath('[1]')
-    offset = con_info_ptr.GetValueForExpressionPath('[1].con_desc').GetValueAsUnsigned()
-    # return base.AddressOf().CreateChildAtOffset('con_desc', offset, char_type)
-    return base.CreateValueFromAddress('con_desc', base.GetLoadAddress()+offset, char_type).AddressOf().GetSummary().strip('"')
+        i = 0
+        while i < ptrs:
+            payload_i = payload.GetValueForExpressionPath('[' + str(i) + ']')
+            self.payload[i] = Closure.get(self.debugger, payload_i)
+            i += 1
 
-def cast_closure(obj):
-    obj = untag_closure(obj)
-    info_table = get_info_table_from_closure(obj)
-    closure_type = info_table.GetChildMemberWithName('type')
-    type_name = closure_type_name(closure_type.GetValueAsUnsigned())
-    target = lldb.debugger.GetSelectedTarget()
-    closure_type = target.FindFirstType(type_name)
-    # propagate the constructor desciption or info table name to the closure
-    name = get_con_desc(info_table) or info_table.GetName()
-    return obj.CreateValueFromAddress(name, obj.GetLoadAddress(), closure_type)
-    # obj.CreateChildAtOffset(name, 0, closure_type)
+        while i < ptrs+nptrs:
+            payload_i = payload.GetValueForExpressionPath('[' + str(i) + ']')
+            self.payload[i] = payload_i.GetValueAsUnsigned() # Closure.get(self.debugger, Closure.untag(self.debugger, payload_i))
+            i += 1
+
+    @staticmethod
+    def untag(debugger, obj):
+        # TODO: untag for integers and pointers
+        closure_type = find_first_type(debugger, 'StgClosure_')
+        return obj.CreateValueFromAddress(obj.GetName() + '_closure', obj.GetValueAsUnsigned() & ~7, closure_type)
+
+    def info_table(self):
+        target = self.debugger.GetSelectedTarget()
+        header = self.obj.GetValueForExpressionPath('.header') # TODO: remove
+        # lldb doesn't support negative subscripts, this would be cleaner as '.header.info[-1]'
+        info_table = self.obj.GetValueForExpressionPath('.header.info')
+        info_table_sym = target.ResolveSymbolContextForAddress(info_table.Dereference().GetAddress(), lldb.eSymbolContextSymbol).GetSymbol()
+        # sym.GetStartAddress().GetLoadAddress(target)
+        # cast it back to the info table
+        stg_info_table_type = find_first_type(self.debugger, 'StgInfoTable_')
+        offset_info_table = info_table.CreateValueFromAddress(info_table_sym.GetName(), info_table.GetValueAsUnsigned() - 16, stg_info_table_type)
+        return InfoTable(self.debugger, offset_info_table)
+
+    @staticmethod
+    def type_name(tag):
+        name = ghc_map.closure_name_map[tag]
+        return ghc_map.closure_type_map[name]
+
+    @staticmethod
+    def get(debugger, obj):
+        obj = Closure.untag(debugger, obj)
+        self = Closure(debugger, obj)
+
+        info_table = self.info_table()
+        closure_type = info_table.type()
+        type_name = Closure.type_name(closure_type.GetValueAsUnsigned())
+        target = debugger.GetSelectedTarget()
+        closure_type = find_first_type(debugger, type_name)
+        # propagate the constructor desciption or info table name to the closure
+        return Closure(debugger, obj.CreateValueFromAddress(str(info_table), obj.GetLoadAddress(), closure_type))
+        # obj.CreateChildAtOffset(name, 0, closure_type)
+
+    @staticmethod
+    def get_expression(expr):
+        frame = lldb.thread.GetSelectedFrame()
+        obj = frame.EvaluateExpression(expr)
+        return Closure.get(lldb.debugger, obj)
+
+class Constructor(Closure):
+    def __init__(self, debugger, obj):
+        super(Closure, self).__init__(debugger, obj)
+        pass
+
+class Function(Closure):
+    def __init__(self, debugger, obj):
+        super(Closure, self).__init__(debugger, obj)
+        pass
+
+class Thunk(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class Selector(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class BCO(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class AP(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class PAP(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class Indirection(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class UpdateFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class CatchFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class UnderflowFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class StopFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class BlockingQueue(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class MVar(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class Array(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class MutableArray(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class IORef(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class WeakRef(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class Primitive(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class MutablePrimitive(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class TSO(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class Stack(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class TRecChunk(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class AtomicallyFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class CatchRetryFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class CatchSTMFrame(Closure):
+    def __init__(self, debugger, obj):
+        pass
+
+class InfoTable(object):
+    def __init__(self, debugger, info_table):
+        self.debugger = debugger
+        self.info_table = info_table
+
+    def __str__(self):
+        return self.con_desc() or self.info_table.GetName()
+
+    def con_desc(self):
+        target = self.debugger.GetSelectedTarget()
+        stg_con_info_table_type = find_first_type(self.debugger, 'StgConInfoTable_')
+        # stg_con_info_table_type = target.FindFirstType('StgConInfoTable_') # waiting on bug 11574
+        con_info = self.info_table.Cast(stg_con_info_table_type)
+        con_info_ptr = con_info.AddressOf()
+        char_type = self.info_table.GetType().GetBasicType(lldb.eBasicTypeChar)
+        base = con_info_ptr.GetValueForExpressionPath('[1]')
+        offset = con_info_ptr.GetValueForExpressionPath('[1].con_desc').GetValueAsUnsigned()
+        summary = base.CreateValueFromAddress('con_desc', base.GetLoadAddress()+offset, char_type).AddressOf().GetSummary()
+        return summary.strip('"') if summary else None
+
+    def type(self):
+        return self.info_table.GetChildMemberWithName('type')
+
+def find_first_type(debugger, type_name):
+    target = debugger.GetSelectedTarget()
+    return target.FindTypes(type_name).GetTypeAtIndex(0)
+
+def print_std_obj_header(obj, tag):
+    # print tag + '('
+    print obj.GetValueForExpressionPath('.header.info')
+    print obj.GetValueForExpressionPath('.header.prof.ccs->cc->label')
+
+def print_std_obj_payload(debugger, obj):
+    target = debugger.GetSelectedTarget()
+    info = ghc.get_info_table_from_closure(obj)
+    ptrs = info.GetValueForExpressionPath('.layout.payload.ptrs').GetValueAsUnsigned()
+    nptrs = info.GetValueForExpressionPath('.layout.payload.nptrs').GetValueAsUnsigned()
+
+    # lldb does bounds checks on array subscripts, which doesn't work with GHC's closure types
+    # so strip the StgClosure[1] type of the array size, leaving StgClosure* (which is not bounds checked)
+    payload = obj.GetValueForExpressionPath('.payload[0]').AddressOf()
+
+    i = 0
+    while i < ptrs:
+        payload_i = payload.GetValueForExpressionPath('[' + str(i) + ']')
+        print str(i) + ': ' + payload_i.GetValue() + ' = ' + get_object_description(untag_closure(payload_i))
+        i += 1
+
+    j = 0
+    while j < nptrs:
+        payload_j = payload.GetValueForExpressionPath('[' + str(i+j) + ']')
+        print str(i+j) + ': ' + payload_j.GetValue() + '#d'
+        j += 1
 
 def print_obj_dbg(debugger, args, result, dict):
     frame = lldb.thread.GetSelectedFrame()
     obj = frame.EvaluateExpression(args) # frame.FindValue(args, lldb.eValueTypeRegister)
-    print cast_closure(obj)
+    obj = Closure.get(debugger, obj)
+    obj.reify()
+    print obj
+    print obj.payload
+    # print_std_obj_payload(obj)
     return None
 
 def __lldb_init_module(debugger, session_dict):
