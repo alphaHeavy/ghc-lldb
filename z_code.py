7858f2ce88046bda0fa0ed15a3f1e79bf67d76a0
Nathan Howell <nhowell@alphaheavy.com>
2011-12-19 14:20:40 -0800

    Add tuple support to the z-coder

--- z_code.py
+++ z_code.py
@@ -1,5 +1,10 @@
 import re
 
+# partial encoding/decoding support for z-coded values, does not support unicode strings
+
+def reverse_dict(obj):
+    return dict((value, key) for key, value in obj.iteritems())
+
 decode_map = {'ZL': '('
              ,'ZR': ')' 
              ,'ZM': '['
@@ -26,9 +31,18 @@ decode_map = {'ZL': '('
              ,'zu': '_'
              ,'zv': '%'}
 
+decode_map['Z0T'] = '()'
+decode_map['Z1H'] = '(# #)'
+for i in range(2, 9):
+    commas = ',' * (i - 1)
+    decode_map['Z{0}T'.format(i)] = '({0})'.format(commas)
+    decode_map['Z{0}H'.format(i)] = '(#{0}#)'.format(commas)
+
+encode_map = reverse_dict(decode_map)
+
 def decode_chunk(chunk):
-    if len(chunk) == 2 and (chunk[0] == 'Z' or chunk[0] == 'z'):
-        return decode_map.get(chunk, chunk[1])
+    if len(chunk) > 1 and (chunk[0] == 'Z' or chunk[0] == 'z'):
+        return decode_map.get(chunk, chunk[1:])
     else:
         return chunk
 
@@ -36,6 +50,15 @@ def decode(str):
     if str == None:
         return None
 
-    chunks = re.split('([zZ].)', str)
+    chunks = re.split('(Z[0-9][TH]|[zZ].)', str)
     return ''.join(map(decode_chunk, chunks))
+
+def encode_char(char):
+    return encode_map.get(char, char)
+
+def encode(str):
+    if str == None:
+        return None
+
+    return ''.join(map(encode_char, str))
 
