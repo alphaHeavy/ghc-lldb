8ecdd669954694053259d86f433d71f084c5a51a
Nathan Howell <nhowell@alphaheavy.com>
2011-12-20 20:59:00 -0800

    Do a slightly better job encoding strings

--- z_code.py
+++ z_code.py
@@ -40,6 +40,8 @@ for i in range(2, 9):
 
 encode_map = reverse_dict(decode_map)
 
+assert len(encode_map) == len(decode_map)
+
 def decode_chunk(chunk):
     if len(chunk) > 1 and (chunk[0] == 'Z' or chunk[0] == 'z'):
         return decode_map.get(chunk, chunk[1:])
@@ -53,12 +55,18 @@ def decode(str):
     chunks = re.split('(Z[0-9][TH]|[zZ].)', str)
     return ''.join(map(decode_chunk, chunks))
 
-def encode_char(char):
-    return encode_map.get(char, char)
+def encode_chunk(chunk):
+    return encode_map.get(chunk, chunk)
 
 def encode(str):
     if str == None:
         return None
 
-    return ''.join(map(encode_char, str))
+    # generate a scanner to tokenize the input string
+    keys = encode_map.keys()
+    keys.sort(lambda x, y: cmp(len(y), len(x)))
+    split_on = '({0})'.format('|'.join(map(re.escape, keys)))
+
+    chunks = re.split(split_on, str)
+    return ''.join(map(encode_chunk, chunks))
 
